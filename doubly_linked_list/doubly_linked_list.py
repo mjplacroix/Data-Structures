4bce5633d50370d48ad7d6a77063182ffe8d0d03
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-06-08 14:19:42 -0400

    passing all 11 tests

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -62,7 +62,9 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -82,23 +84,61 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return node
+        self.add_to_head(node.value)
+        self.delete(node)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return node
+        self.add_to_tail(node.value)
+        self.delete(node)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        self.length -= 1
+        # this is the only node
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        # it's the head
+        elif node is self.head:
+            self.head = node.next
+            """ how are the following delete() not recursive? Would that require self.delete()?
+            Is this a built in function?
+            What is delete doing? It setting it equal to None?
+            
+            Neither, looks like it's from a function above - it's a method on/in the Node class"""
+            node.delete()
+        # it's the tail
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        # create max var
+        current = self.head
+        max = self.head.value
+        # loop through the nodes
+        while current is not None:
+            # compare value in node to max found
+            if current.value > max:
+            # reassign if greater
+                max = current.value
+            current = current.next
+        # return max found
+        return max
